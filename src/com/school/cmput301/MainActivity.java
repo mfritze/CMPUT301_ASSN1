518cc68b18fcf86a59e3816fb7b427c3474e0f79
mfritze <mfritze@ualberta.ca>
2015-01-19 15:20:16 -0700

    Added prelimanary array adapter code

--- src/com/school/cmput301/MainActivity.java
+++ src/com/school/cmput301/MainActivity.java
@@ -1,11 +1,15 @@
 package com.school.cmput301;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 	
@@ -14,6 +18,10 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		ListView claimView = (ListView) findViewById(R.id.claimListView);
+		ArrayList<Claim> claims = ClaimListSingleton.getClaimList();
+		ArrayAdapter<Claim> claimAdapter = new ArrayAdapter<Claim>(this, android.R.layout.simple_list_item_1 , claims);
+		claimView.setAdapter(claimAdapter);
 	}
 
 	@Override
